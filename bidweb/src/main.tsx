562fe7c98329a4dcacb2594b537d009062c07d58
dandgavhalsakshi <sakshidandgavhal71@gmail.com>
2025-02-28 11:28:02 +0530

    sakshi #1

--- bidweb/src/main.tsx
+++ bidweb/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import './Components/ProductDetailPage.tsx'
+import './Components/FastrackAuctionPage.tsx'
+// import App from './App.tsx'
+import FastrackAuctionPage from './Components/FastrackAuctionPage.tsx'
+import ProductDetailPage from './Components/ProductDetailPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FastrackAuctionPage />
+    <ProductDetailPage/>
   </StrictMode>,
 )
