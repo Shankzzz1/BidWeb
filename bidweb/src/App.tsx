f858a4d1e7af95b22fa50f38fde75b3df2f76762
autitanuja <tanuja12114@gmail.com>
2025-03-02 12:40:38 +0530

    tanuja #2

--- bidweb/src/App.tsx
+++ bidweb/src/App.tsx
@@ -1,7 +1,12 @@
+import AuctionForms from "./Components/AuctionForms";
+
 export default function App() {
   return (
     <>
-    
+    <AuctionForms />
     </>
   )
 }
+
+
+
--- bidweb/src/main.tsx
+++ bidweb/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import './Components/AuctionForms.tsx'
+// import App from './App.tsx'
+import AuctionForms from './Components/AuctionForms.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    < AuctionForms/>
+
   </StrictMode>,
 )
