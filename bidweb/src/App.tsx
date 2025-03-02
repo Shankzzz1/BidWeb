b9c37e5a6a301df69d02c50c0ced6da772c8b234
dandgavhalsakshi <sakshidandgavhal71@gmail.com>
2025-03-02 12:36:53 +0530

    #6 sakshi

--- bidweb/src/App.tsx
+++ bidweb/src/App.tsx
@@ -8,7 +8,7 @@ import About from "./Components/About";
 import ScrollablePages from './Components/ScrollablePages'
 import ProductDetailPage from "./Components/ProductDetailPage";
 import ProfilePage from "./Components/ProfilePage";
-import ProductPage from "./Components/ProductPage";
+
 export default function App() {
   return (
     <Router>
