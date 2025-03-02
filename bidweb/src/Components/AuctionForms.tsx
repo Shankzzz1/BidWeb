622df36374e3dc43b315fac61f006ea932e2ae44
autitanuja <tanuja12114@gmail.com>
2025-03-02 12:46:39 +0530

    Tanuja # 3

--- bidweb/src/Components/AuctionForms.tsx
+++ bidweb/src/Components/AuctionForms.tsx
@@ -329,7 +329,7 @@ const AuctionForms = () => {
               disabled={isSubmitting}
               className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
             >
-              {isSubmitting ? 'Searching...' : 'Search Auctions'}
+              {isSubmitting ? 'Searching....' : 'Search Auctions'}
             </button>
           </div>
         </form>
